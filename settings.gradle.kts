f75e7d93d48745b6abc968bbe2c5717f17f4b5b2
DavidMTomas <143930697+DavidMTomas@users.noreply.github.com>
2024-10-09 19:31:40 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Tema3Layouts"
+include(":app")
+include(":app:linearlayout")
+include(":app:framelayout")
